15b41ac10a46b1521cb75b0bb2a4091a68ff5d62
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-07-30 11:35:25 -0400

    Add files via upload

--- ms2020/shared/robot_setup.py
+++ ms2020/shared/robot_setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 # from pybricks.hubs import EV3Brick
+import pybricks.nxtdevices as nxt
 from pybricks import ev3brick as brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                 InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -20,14 +21,15 @@ WHEEL_CIRCUM_MM=3.149*89
 DEGREES_PER_MM=360/WHEEL_CIRCUM_MM
  
 #drive motors
-left_motor=Motor(Port.D, Direction.CLOCKWISE)
-right_motor=Motor(Port.C, Direction.CLOCKWISE)
+left_motor= Motor(Port.D, Direction.CLOCKWISE)
+right_motor= Motor(Port.C, Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor, WHEEL_DIAMETER_MM, AXLE_TRACK_MM)
-crane_motor=None ### Motor(Port.B, Direction.CLOCKWISE, [8,24])
-rack_motor=None ####  Motor(Port.A, Direction.CLOCKWISE)
+crane_motor= Motor(Port.A, Direction.CLOCKWISE, [8,24])
+rack_motor=None ####  Motor(Port.B, Direction.CLOCKWISE)
 
-gyro=GyroSensor(Port.S1, Direction.COUNTERCLOCKWISE)
+gyro= GyroSensor(Port.S1, Direction.COUNTERCLOCKWISE)
 color_sensor_left = None ### ColorSensor(Port.S1)
 color_sensor_right = None ### ColorSensor(Port.S4)
 color_sensor_center = ColorSensor(Port.S4)
-touch_sensor= TouchSensor(Port.S3)
+touch_sensor= None ##TouchSensor(Port.S3)
+ultrasound=  None ##nxt.UltrasonicSensor(Port.S2)
