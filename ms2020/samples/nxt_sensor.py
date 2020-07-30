cbd8da6104b1889b52829d6570e9c4d8337fa6f7
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-07-30 11:40:20 -0400

    Add files via upload

--- ms2020/samples/nxt_sensor.py
+++ ms2020/samples/nxt_sensor.py
@@ -1,15 +1,67 @@
 #!/usr/bin/env pybricks-micropython
 # import ev3dev.ev3 as ev3
-import pybricks.nxtdevices as nxt
-# from pybricks.nxtdevices import UltrasonicSensor
-import pybricks.ev3devices as ev3dev
-from pybricks.tools import wait, StopWatch
-from pybricks import ev3brick as brick
-from pybricks.ev3devices import UltrasonicSensor,TouchSensor
-from pybricks.parameters import Port, Stop, Direction, Button, Color
+# import sys
+# import os
+# import pybricks.nxtdevices as nxt
+# # from pybricks.nxtdevices import UltrasonicSensor
+# import pybricks.ev3devices as ev3dev
+# from pybricks.tools import wait, StopWatch
+# from pybricks import ev3brick as brick
+# from pybricks.ev3devices import UltrasonicSensor,TouchSensor
+# from pybricks.parameters import Port, Stop, Direction, Button, Color
+# sys.path.append('../shared')
+ 
+# import robot_setup
 
-# us = ev3.UltrasonicSensor()
-us = nxt.UltrasonicSensor(Port.S1)
+# # us = ev3.UltrasonicSensor()
+# # us = nxt.UltrasonicSensor(Port.S2)
+
+
+
+
+
+
+import sys
+import os
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                               InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.ev3devices import Motor
+ 
+sys.path.append('../shared')
+import robot_setup
+ 
+from robot_setup import left_motor
+from robot_setup import right_motor
+from robot_setup import robot
+from robot_setup import rack_motor
+from robot_setup import crane_motor
+from robot_setup import gyro
+from robot_setup import touch_sensor
+from robot_setup import color_sensor_left
+from robot_setup import color_sensor_right
+from robot_setup import color_sensor_center
+from robot_setup import touch_sensor
+from robot_setup import ultrasound
+ 
+from robot_setup import SOUND_VOLUME
+from robot_setup import WHEEL_DIAMETER_MM
+from robot_setup import AXLE_TRACK_MM
+from robot_setup import SENSOR_TO_AXLE
+from robot_setup import WHEEL_CIRCUM_MM
+from robot_setup import DEGREES_PER_MM
+ 
+##### Do not change above this line ##########################################
+
+
+
+
+
+
+
 color_sensor_center = None##ColorSensor(Port.S4)
 touch_sensor= None## TouchSensor(Port.S2)
 message='Staring ultrasomic'
@@ -19,8 +71,8 @@ brick.display.text(message)
 
 def test_sensor():
     # while touch_sensor.pressed() == False :
-    while us.distance() > 10:
-        message='Distance ' + str(us.distance())
+    while ultrasound.distance() > 10:
+        message='Distance ' + str(ultrasound.distance())
         print(message)
         brick.display.text(message)
         wait(1000)
