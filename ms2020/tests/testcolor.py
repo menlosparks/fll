48326fdf8e23d02c94d1e36a41995f097793d21c
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-09-13 12:34:10 -0400

    Add files via upload

---
+++ ms2020/tests/testcolor.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env pybricks-micropython
+
+import sys
+import os
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                               InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.ev3devices import Motor
+ 
+sys.path.append('../shared')
+sys.path.append('../samples')
+import robot_setup
+ 
+from robot_setup import left_motor
+from robot_setup import right_motor
+from robot_setup import robot
+from robot_setup import rack_motor
+from robot_setup import crane_motor
+from robot_setup import gyro
+from robot_setup import touch_sensor
+from robot_setup import color_sensor_left
+from robot_setup import color_sensor_right
+from robot_setup import color_sensor_center
+from robot_setup import touch_sensor
+from robot_setup import ultrasound
+ 
+from robot_setup import SOUND_VOLUME
+from robot_setup import WHEEL_DIAMETER_MM
+from robot_setup import AXLE_TRACK_MM
+from robot_setup import SENSOR_TO_AXLE
+from robot_setup import WHEEL_CIRCUM_MM
+from robot_setup import DEGREES_PER_MM
+ 
+import shared_all
+
+##### Do not change above this line ##########################################
+
+
+def color_test(distance_mm, speed_mm_s):
+
+    left_motor.reset_angle(0)
+    motor_target_angle = int(DEGREES_PER_MM * distance_mm)
+
+    while (abs(left_motor.angle()) < abs(motor_target_angle)):
+        shared_all.log_string(
+            'Center : ' + str(color_sensor_center.color()) + '(' + str(color_sensor_center.reflection()) + ')' 
+            + 'Right : ' + str(color_sensor_right.color()) + '(' + str(color_sensor_right.reflection()) + ')' 
+            )
+        robot.drive(speed_mm_s, 0)
+        wait(200)
+
+    robot.stop(stop_type=Stop.BRAKE)
+
+color_test( distance_mm=100, speed_mm_s=50)
+wait(999999)
