f1ba2a85488ea635b68170c07a549f49ff95ee39
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-10-17 13:41:38 -0400

    Add files via upload

--- ms2020/missions/slide.py
+++ ms2020/missions/slide.py
@@ -20,7 +20,7 @@ from robot_setup import rack_motor
 from robot_setup import crane_motor
 from robot_setup import gyro
 from robot_setup import touch_sensor
-from robot_setup import color_sensor_back
+from robot_setup import color_sensor_left
 from robot_setup import color_sensor_right
 from robot_setup import color_sensor_center
 from robot_setup import touch_sensor
@@ -38,24 +38,18 @@ import shared_all
 ##### Do not change above this line ##########################################
 
 def run (): 
-    shared_all.move_rack_to_floor ()
+    shared_all.move_crane_to_floor (motor=rack_motor)
     shared_all.move_straight(distance_mm=50, speed_mm_s=120)
-    
     while shared_all.did_motor_stall(motor =rack_motor , max_degrees =30 , speed = 80):
-        shared_all.move_straight(distance_mm=6, speed_mm_s=-20)
-
-    shared_all.move_crane_up (motor=rack_motor, degrees=25)
-    shared_all.drive_raising_crane (duration_ms=900, robot_distance_mm=20, robot_turn_angle=0, 
-                        motor=rack_motor, crane_angle=20)
-    shared_all.move_crane_to_angle(motor=rack_motor, target_angle=75)
-    shared_all.drive_raising_crane (duration_ms=900, robot_distance_mm=40, robot_turn_angle=0, 
+        shared_all.move_straight(distance_mm=4, speed_mm_s=-20)
+    
+    shared_all.move_crane_up (motor=rack_motor, degrees=40)
+    shared_all.drive_raising_crane (duration_ms=700, robot_distance_mm=5, robot_turn_angle=0, 
+                        motor=rack_motor, crane_angle=60)
+    shared_all.drive_raising_crane (duration_ms=700, robot_distance_mm=40, robot_turn_angle=0, 
                         motor=rack_motor, crane_angle=-20)
-
-
-## Below lines only for testing
+      ## Below lines only for testing
 ## Comment out when done testing. Do not upload to Git hub without commenting.
 shared_all.calibrate_gyro(-90)
 # align()
 run()
-
-
