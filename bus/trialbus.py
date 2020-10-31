60f7cf940cdd6a6e8dc4358e6477abb73c87f5e6
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-10-31 16:31:17 -0400

    Add files via upload

---
+++ bus/trialbus.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env pybricks-micropython
+ 
+import sys
+import os
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                               InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.ev3devices import Motor
+ 
+sys.path.append('../shared')
+import robot_setup
+import shared_all
+ 
+from robot_setup import left_motor
+from robot_setup import right_motor
+from robot_setup import robot
+from robot_setup import rack_motor
+from robot_setup import crane_motor
+from robot_setup import gyro
+from robot_setup import touch_sensor
+from robot_setup import color_sensor_left
+from robot_setup import color_sensor_right
+from robot_setup import color_sensor_center
+from robot_setup import touch_sensor
+from robot_setup import ultrasound
+ 
+from robot_setup import SOUND_VOLUME
+from robot_setup import WHEEL_DIAMETER_MM
+from robot_setup import AXLE_TRACK_MM
+from robot_setup import SENSOR_TO_AXLE
+from robot_setup import WHEEL_CIRCUM_MM
+from robot_setup import DEGREES_PER_MM
+ 
+##### Do not change above this line ##########################################
+
+
+def base_to_pullup():
+    shared_all.move_straight_target_direction(gyro = gyro, 
+            distance_mm=350, 
+            speed_mm_s=120, 
+        target_angle= 0)
+        
+
+
+def pullup_to_dance():
+    shared_all.move_straight_target_direction(gyro = gyro, 
+            distance_mm=150, 
+            speed_mm_s=180, 
+        target_angle= -90)
+    shared_all.turn_arc(distance=150, angle = -60, speed_mm_s=130)
+    shared_all.turn_arc(distance=150, angle = 60, speed_mm_s=130)
+
+
+
+## Below lines only for testing
+## Comment out when done testing. Do not upload to Git hub without commenting.
+
+# Calibrate the gyro point in the direction at the start 
+shared_all.calibrate_gyro(0)
+
+base_to_pullup()
