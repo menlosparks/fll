5f2fe0cd9a820a145f326da3d9e4d9920539ccac
menlosparks <66791099+menlosparks@users.noreply.github.com>
2020-12-05 15:11:44 -0500

    Add files via upload

---
+++ ms2020/bus/ArjunBus.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env pybricks-micropython
+# from pybricks.hubs import EV3Brick
+import sys
+import os
+import inspect
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.ev3devices import Motor
+
+sys.path.append('../shared')
+import shared_all
+import robot_setup
+
+from robot_setup import left_motor
+from robot_setup import right_motor
+from robot_setup import robot
+from robot_setup import rack_motor
+from robot_setup import crane_motor
+#from robot_setup import gyro
+from robot_setup import touch_sensor 
+#from robot_setup import color_sensor_left
+from robot_setup import color_sensor_right
+from robot_setup import color_sensor_center
+from robot_setup import touch_sensor
+
+from robot_setup import SOUND_VOLUME
+from robot_setup import WHEEL_DIAMETER_MM
+from robot_setup import AXLE_TRACK_MM
+from robot_setup import SENSOR_TO_AXLE
+from robot_setup import WHEEL_CIRCUM_MM
+from robot_setup import DEGREES_PER_MM
+
+##### Do not change above this line ##########################################
+ 
+
+def basetotread ():
+    shared_all.move_straight(distance_mm=1390, speed_mm_s=300)
+
+def inovtobball ():
+    shared_all.move_straight(distance_mm=80, speed_mm_s=-100)
+    shared_all.turn(-90)
+    shared_all.move_straight(distance_mm=490, speed_mm_s=150)
+    shared_all.turn(-50)
+    shared_all.move_straight(distance_mm=50, speed_mm_s=75)
+
